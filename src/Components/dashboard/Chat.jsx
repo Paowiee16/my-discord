c47962e304ee2a1f59c81902420fe08de2fcf30a
Adopt-Pet <celine.nvalencia@gmail.com>
2023-06-14 21:40:39 +0800

    Remove icon

--- src/Components/dashboard/Chat.jsx
+++ src/Components/dashboard/Chat.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { db, auth } from "../../config/firebase";
 import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
+import SendMessage from "./SendMessage";
 
 function Chat() {
   const [messages, setMessages] = useState([]);
   useEffect(() => {
+    //connects to DB and list the chats based on time created.
     const q = query(collection(db, "chat"), orderBy("createdAt"));
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       let messages = [];
@@ -17,14 +19,20 @@ function Chat() {
   }, []);
 
   return (
-    <div>
-      Chat:
-      {messages.map((message) => (
-        <div key={message.id}>
-          <p>{message.message}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className=" flex flex-col items-end justify-end gap-4  w-screen ">
+        Chat:
+        {messages.map((message) => (
+          <div
+            key={message.id}
+            className="  py-3 px-4 bg-slate-600 rounded-lg  "
+          >
+            <p>{message.message}</p>
+          </div>
+        ))}
+        <SendMessage />
+      </div>
+    </>
   );
 }
 
